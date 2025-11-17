e7815ad09b1e9d6aeab4501b4824944e6e5e5554
sina <sina.tabriziyan@gmail.com>
2025-11-17 22:11:51 +0330

    init app

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Holoo"
+include(":app")
+include(":core:data")
+include(":core:domain")
+include(":feature:kiosk")
+include(":feature:restaurant")
+include(":core:common")
